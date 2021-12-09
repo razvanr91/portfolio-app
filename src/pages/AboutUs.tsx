b23061c70ff8b534d16162394d2fd4580635d24d
razvanr91 <razvan.roman91@outlook.com>
2021-12-09 15:26:11 +0000

    Create FAQ section

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 // Page Components
 import AboutSection from "../components/AboutSection";
 import CompSection from "../components/CompSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
 	return (
 		<Fragment>
 			<AboutSection />
 			<CompSection />
+			<FaqSection />
 		</Fragment>
 	);
 };
