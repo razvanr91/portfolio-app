3ba2e760ee1c7337ff5a5860390542e4d826a777
razvanr91 <razvan.roman91@outlook.com>
2021-12-09 16:31:36 +0000

    Add global and about section styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+// Global Styles
+import GlobalStyle from "./components/GlobalStyles";
 // Import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
 	return (
 		<div className="App">
+			<GlobalStyle />
 			<AboutUs />
 		</div>
 	);
