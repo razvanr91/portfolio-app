b10d18deb2178ac5b6e2a0c60f11474236584fca
razvanr91 <razvan.roman91@outlook.com>
2021-12-10 23:46:03 +0000

    Animate About page elements

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -6,6 +6,7 @@ import { About, Description, Hide, Image } from "../styles";
 
 // Framer Motion
 import { motion } from "framer-motion";
+import { titleAnimation, fade, photoAnimation } from "../animation";
 
 const AboutSection = () => {
 	return (
@@ -13,22 +14,24 @@ const AboutSection = () => {
 			<Description>
 				<motion.div className="title">
 					<Hide>
-						<motion.h2>We work to make</motion.h2>
+						<motion.h2 variants={titleAnimation}>We work to make</motion.h2>
 					</Hide>
 					<Hide>
-						<motion.h2>
+						<motion.h2 variants={titleAnimation}>
 							your <span>dreams</span> come
 						</motion.h2>
 					</Hide>
 					<Hide>
-						<motion.h2>true.</motion.h2>
+						<motion.h2 variants={titleAnimation}>true.</motion.h2>
 					</Hide>
 				</motion.div>
-				<p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills!</p>
-				<button>Contact Us</button>
+				<motion.p variants={fade}>
+					Contact us for any photography or videography ideas that you have. We have professionals with amazing skills!
+				</motion.p>
+				<motion.button variants={fade}>Contact Us</motion.button>
 			</Description>
 			<Image>
-				<img src={home1} alt="guy with camera" />
+				<motion.img variants={photoAnimation} src={home1} alt="guy with camera" />
 			</Image>
 		</About>
 	);
